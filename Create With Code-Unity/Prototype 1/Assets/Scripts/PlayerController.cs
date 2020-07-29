ba0f1aaa38bc54af3207c5a419aa3acca2283609
MelihCelik00 <melihsafa.c@gmail.com>
2020-07-29 18:41:10 +0300

    update

--- Create With Code-Unity/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code-Unity/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField] private int speedOffset;
+    void Update()
+    {
+        // The vehicle will move
+        transform.Translate(Vector3.forward * (Time.deltaTime * speedOffset));
+    }
 }

6673dfcb25c74ddcf8f6755e9d989134fa5c44b9
MelihCelik00 <melihsafa.c@gmail.com>
2020-07-30 12:00:32 +0300

    vehicle control update

---
+++ Create With Code-Unity/Prototype 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject vehicle;
+    [SerializeField] private Vector3 OffSetDistance;
+    
+    
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = vehicle.transform.position + OffSetDistance;
+    }
+}
--- Create With Code-Unity/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code-Unity/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int speedOffset;
+
+    [SerializeField] private float turnSpeed;
+
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
+
+    private void Start()
+    {
+        
+    }
+
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
         // The vehicle will move
-        transform.Translate(Vector3.forward * (Time.deltaTime * speedOffset));
+        transform.Translate(Vector3.forward * (Time.deltaTime * speedOffset * verticalInput));
+        transform.Translate(Vector3.right * (Time.deltaTime * turnSpeed * horizontalInput));
     }
 }

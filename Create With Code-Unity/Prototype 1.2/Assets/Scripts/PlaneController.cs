080566f981b78d4b17ced95c24889877bf8257cc
MelihCelik00 <melihsafa.c@gmail.com>
2020-07-30 14:19:36 +0300

    plane prototype

---
+++ Create With Code-Unity/Prototype 1.2/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    [SerializeField] private int turnSpeed;
+    [SerializeField] private int altitudeControlSpeed;
+    [SerializeField] private int velocity;
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+        
+        CheckMovementMethods();
+    }
+
+    private void CheckMovementMethods()
+    {
+        Move();
+        Turn();
+        AltitudeControl();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveAmongTheLocalAxis(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveAmongTheLocalAxis(Vector3.back);
+        }
+    }
+
+    private void Turn()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            HorizontalRotation(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            HorizontalRotation(Vector3.forward);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            VerticalRotation(Vector3.down);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            VerticalRotation(Vector3.up);
+        }
+    }
+
+    private void AltitudeControl()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            VerticalRotation(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            VerticalRotation(Vector3.right);
+        }
+    }
+
+    private void MoveAmongTheLocalAxis(Vector3 side)
+    {
+        transform.Translate(side * (Time.deltaTime * velocity));
+    }
+
+    private void HorizontalRotation(Vector3 horizontalDirection)
+    {
+        transform.Rotate(horizontalDirection * (Time.deltaTime * turnSpeed * _horizontalInput * -1));
+    }
+
+    private void VerticalRotation(Vector3 verticalDirection)
+    {
+        transform.Rotate(verticalDirection * (Time.deltaTime * turnSpeed * _verticalInput));
+    }
+
+}
---
+++ Create With Code-Unity/Prototype 1.2/Assets/Scripts/PlaneFollower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject _vehicle;
+    [SerializeField] private Vector3 cameraDistanceOffset;
+    
+    void Update()
+    {
+        transform.position = _vehicle.transform.position + cameraDistanceOffset;
+    }
+}
